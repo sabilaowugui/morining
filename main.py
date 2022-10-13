d017572113ef7901c78277b1687f16b87b38adf6
sabilaowugui <110769028+sabilaowugui@users.noreply.github.com>
2022-10-13 11:21:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def get_birthday_1():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  words = requests.get("http://api.tianapi.com/caihongpi/index")
   if words.status_code != 200:
     return get_words()
   return words.json()['data']['text']
