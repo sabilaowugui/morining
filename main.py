0d605d28befd13f5fc757b19a36de496ea5930de
sabilaowugui <110769028+sabilaowugui@users.noreply.github.com>
2023-04-17 14:28:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url1 = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  ur1 = "https://api.seniverse.com/v3/weather/daily.json?key=SJ1KahA0xmlMbWYzi&language=zh-Hans&unit=c&start=-1&days=5&location= " + city
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=204182891&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
