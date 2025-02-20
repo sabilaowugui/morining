bf5f98968b70d23deb2cbc99c51c38c153cff0e1
sabilaowugui <110769028+sabilaowugui@users.noreply.github.com>
2025-02-20 11:54:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_words():
   words = requests.get("https://apis.tianapi.com/caihongpi/index?key=ac823a02d471776347fcf7a71bd91794")
   if words.status_code != 200:
     return get_words()
-  return words.json()['data']['text']
+  return words.json()['result']['content']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
