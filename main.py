08f5d534344e304b9a7886869ee431fd26c6b0cb
sabilaowugui <110769028+sabilaowugui@users.noreply.github.com>
2025-02-27 10:50:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,8 @@ def get_words():
 #    return get_words()
 #  return words.json()['data']['text']
 
-#def get_random_color():
-#  return "#%06x" % random.randint(0, 0xFFFFFF)
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
